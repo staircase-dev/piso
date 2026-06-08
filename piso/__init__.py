e892d085cd6b762c73ff50008b50dfc90b3d6df4
Venaturum <venaturum@gmail.com>
2026-06-08 19:51:58 +1000

    Support for pandas 3 and Python 3.14 (#68)

--- piso/__init__.py
+++ piso/__init__.py
@@ -46,24 +46,12 @@ def register_accessors():
 
 
 def get_version():
-    def get_version_post_py38():
+    try:
         from importlib.metadata import version  # type: ignore
 
         return version(__name__)
-
-    def get_version_pre_py38():
-        from pkg_resources import get_distribution
-
-        return get_distribution(__name__).version
-
-    def default_version():
+    except Exception:
         return "unknown"
-
-    for func in (get_version_post_py38, get_version_pre_py38, default_version):
-        try:
-            return func()
-        except Exception:
-            pass
 
 
 __version__ = get_version()
